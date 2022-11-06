89e704745b06695e397751c08750df798dcff4ea
Sergey Voronin <Sergey.Voronin@axpo.com>
2022-11-06 22:10:42 +0100

    update locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser, task, between
 
+
 class WebsiteTestUser(HttpUser):
     wait_time = between(0.5, 3.0)
 
@@ -8,5 +9,24 @@ class WebsiteTestUser(HttpUser):
         self.client.get("https://flaskml-sergey-voronin.azurewebsites.net")
 
     @task(2)
-    def test2(self):
-        self.client.post("https://flaskml-sergey-voronin.azurewebsites.net:443/predict")
+    def checkPredict(self):
+        self.client.post("/predict", json={
+            "CHAS": {
+                "0": 0
+            },
+            "RM": {
+                "0": 6.575
+            },
+            "TAX": {
+                "0": 296.0
+            },
+            "PTRATIO": {
+                "0": 15.3
+            },
+            "B": {
+                "0": 396.9
+            },
+            "LSTAT": {
+                "0": 4.98
+            }
+        })
